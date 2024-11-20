7a4e281b6cba5543cce9b7ef02f1ba5e9856344a
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-20 19:17:49 +0300

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import LanguageSelector from '@/components/ui/dropdowns/LanguageSelector.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <!-- <SearchInput :placeholder="$t('header.search')"></SearchInput>
-    <IconButton :src="'images/icons/search-icon.svg'" :onclick="test"></IconButton> -->
-    <LanguageSelector></LanguageSelector>
-  </main>
+  <main></main>
 </template>
 
 <script lang="ts">
