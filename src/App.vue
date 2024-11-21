d94a68da730dfa8ad4b7588737d373aacbafade2
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-11-21 06:52:26 +0300

    Update default

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,33 @@ import Header from '@/components/layouts/Header.vue'
 
 <template>
   <Header></Header>
-  <RouterView />
+  <div class="content">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100%;
+  min-height: 100vh;
+  margin-top: 200px;
+  background-color: var(--dark-background);
+}
+
+.container {
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@media only screen and (max-width: 1000px) {
+  .container {
+    margin-top: 240px;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .container {
+    margin-top: 260px;
+  }
+}
+</style>
