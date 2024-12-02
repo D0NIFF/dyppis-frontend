ab638a1f2ff00a245ffdfc36407eb1bc082aea7e
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-12-02 19:21:27 +0300

    Create compotonet for the letters

---
+++ src/components/ui/lists/LetterList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="list">
+    <div class="letter" v-for="letter in letters" :key="letter" @click="this.letterClick(letter)">
+      <p>{{ letter }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      letters: [
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+      ],
+    }
+  },
+  methods: {
+    letterClick(elem) {
+      console.log(elem)
+    },
+  },
+  props: {
+    onclick: {
+      type: Function,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+}
+.letter {
+  padding: 10px;
+  color: white;
+  background-color: var(--dark-light-background);
+  transition: 0.2s ease-in-out;
+  cursor: pointer;
+}
+.letter:hover {
+  background-color: var(--dark-light-background-hover);
+}
+</style>
